85ab27e8a133eb8e1301759265a37f36254428c9
LordPeNTester <bugbot404@users.noreply.github.com>
2021-03-01 22:24:56 +0530

    Android Module

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request
-import os
 from pytube import YouTube
+from android.storage import primary_external_storage_path
+from android.permissions import request_permissions, Permission
+
+request_permissions([Permission.WRITE_EXTERNAL_STORAGE])
+
+primary_ext_storage = primary_external_storage_path()
+
 
 app = Flask(__name__)
-
-user = os.getlogin()
-path = r'c:\Users\{}\Downloads'.format(user)
 
 @app.route('/')
 def home():
@@ -16,7 +19,7 @@ def download():
     global path
     url = request.form['link']
     ytd = YouTube(url)
-    music1 = ytd.streams.filter(only_audio=True, file_extension='mp4').first().download(path)
+    music1 = ytd.streams.filter(only_audio=True, file_extension='mp4').first().download(primary_ext_storage)
     return music1
 
 
